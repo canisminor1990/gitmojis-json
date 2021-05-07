d404c4ffad39fda6e74211a8f2bf98063de12b67
倏昱 <yufan.yyf@antgroup.com>
2021-05-07 17:50:57 +0800

    📝 docs: add README.md
    #

---
+++ README.sh
@@ -0,0 +1,13 @@
+# gitmojis-json
+
+## usage
+
+```bash
+curl -L https://raw.githubusercontent.com/canisminor1990/gitmojis-json/master/install.sh|bash
+```
+
+or
+
+```bash
+git clone https://github.com/canisminor1990/gitmojis-json.git && cd gitmojis-json && mv gitmojis.json ~/.gitmoji/gitmojis.json
+```
