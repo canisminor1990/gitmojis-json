dc6a480d9809b4f52e54473089a82fcf75a3d2f1
倏昱 <yufan.yyf@antgroup.com>
2021-05-07 17:32:26 +0800

    🐛 fix: fix curl
    #

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "😘 开始替换 gitmojis.json 文件"
 cd ~/.gitmoji/
-curl https://raw.githubusercontent.com/canisminor1990/gitmojis-json/master/gitmojis.json >> gitmojis.json
+curl -o gitmojis.json https://raw.githubusercontent.com/canisminor1990/gitmojis-json/master/gitmojis.json
 echo "✅ 替换成功"
 gitmojis -l
