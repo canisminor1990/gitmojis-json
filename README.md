11865ebdaf6ff0e3636cd3c0ca5ddcd89cafdd4b
canisminor1990 <i@canisminor.cc>
2023-05-09 23:03:34 +0800

    ✨ feat: add gitmojis-lite

--- README.md
+++ README.md
@@ -3,11 +3,5 @@
 ## usage
 
 ```bash
-curl -L https://raw.githubusercontent.com/canisminor1990/gitmojis-json/master/install.sh|bash
-```
-
-or
-
-```bash
 git clone https://github.com/canisminor1990/gitmojis-json.git && cd gitmojis-json && cp gitmojis.json ~/.gitmoji/gitmojis.json
 ```
--- install.sh
+++
@@ -1,9 +0,0 @@
-echo "=============================="
-echo "😘 开始替换 gitmojis.json 文件"
-echo "=============================="
-cd ~/.gitmoji/
-curl -o gitmojis.json https://raw.githubusercontent.com/canisminor1990/gitmojis-json/master/gitmojis.json
-echo "=============================="
-echo "✅ 替换成功"
-echo "=============================="
-gitmoji -l
